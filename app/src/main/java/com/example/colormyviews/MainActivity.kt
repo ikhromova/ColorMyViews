615d6544ca0367aef6f8cc7e48c7d2939f53b8f2
Irina.Khromova <irina.chrome@gmail.com>
2021-09-21 20:30:59 +0300

    Add random colors

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,47 +1,49 @@
 package com.example.colormyviews
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.util.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val rootConstraintLayout: ConstraintLayout by lazy { findViewById(R.id.constraint_layout) }
+    private val boxOneText: TextView by lazy { findViewById(R.id.box_one_text) }
+    private val boxTwoText: TextView by lazy { findViewById(R.id.box_two_text) }
+    private val boxThreeText: TextView by lazy { findViewById(R.id.box_three_text) }
+    private val boxFourText: TextView by lazy { findViewById(R.id.box_four_text) }
+    private val boxFiveText: TextView by lazy { findViewById(R.id.box_five_text) }
+    private val boxSixText: TextView by lazy { findViewById(R.id.box_six_text) }
+    private val boxSevenText: TextView by lazy { findViewById(R.id.box_seven_text) }
+    private val boxEightText: TextView by lazy { findViewById(R.id.box_eight_text) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setListeners()
     }
 
-    private fun makeColored(view: View) {
-        when (view.id) {
-            // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(Color.LTGRAY)
-        }
-    }
-
     private fun setListeners() {
-        val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
-
-        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
-
         val clickableViews: List<View> =
-            listOf(
-                boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
-            )
+            listOf(rootConstraintLayout, boxOneText, boxTwoText, boxThreeText,boxFourText, boxFiveText, boxSixText, boxSevenText, boxEightText)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
+    }
+
+    private fun makeColored(view: View) {
+        val color = if (view == rootConstraintLayout) Color.LTGRAY else getRandomColor()
+        return view.setBackgroundColor(color)
+    }
+
+    private fun getRandomColor(): Int {
+        val rnd = Random
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
     }
 }
